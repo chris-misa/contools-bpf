5ce06bb40433298282503b94e722f9b7c605a229
chmisa <chmisa@node1.trcrt.contools-pg0.utah.cloudlab.us>
2018-10-01 00:56:13 -0600

    Working on getting into skb structure or getting the device name in some way

--- test.py
+++ test.py
@@ -1,10 +1,24 @@
 from bcc import BPF
 
 prog='''
+
+#include <linux/skbuff.h>
+#include <linux/netdevice.h>
+
 TRACEPOINT_PROBE(net, netif_receive_skb) {
-  bpf_trace_printk("Hello receive\\n");
+  struct sk_buff *skb = NULL;
+  struct net_device *dev = NULL;
+  char dev_name[IFNAMSIZ] = {};
+  // bpf_probe_read(skb, sizeof(struct sk_buff), args->skbaddr);
+  // bpf_probe_read(dev, sizeof(struct net_device), skb->dev);
+  // bpf_probe_read(dev_name, IFNAMSIZ, dev->name);
+
+  // bpf_trace_printk("skb: %p, dev: %p, name: %s\\n", skb, dev, dev_name);
+
+  bpf_trace_printk("%p\\n", args->skbaddr);
   return 0;
 }
+
 '''
 
 b = BPF(text=prog)
