b5c67b3af676602cd2a9d387b2e6e2ae9b809530
chmisa <chmisa@node1.bpf.contools-pg0.utah.cloudlab.us>
2018-10-04 01:03:45 -0600

    Moved to perf ring buffer

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 from bcc import BPF
+import ctypes as ct
 
 prog='''
 #include <uapi/linux/ptrace.h>
 #include <linux/skbuff.h>
 #include <linux/netdevice.h>
+
+struct latency_t {
+    u64 ts;
+    u64 ns;
+    u32 dir; // 1: send, 2: recv
+};
+BPF_PERF_OUTPUT(events);
 
 BPF_HASH(start, u64);
 
@@ -12,6 +20,7 @@ int do_recv(struct pt_regs *ctx, struct sk_buff *skb)
 {
     int dev_index = skb->dev->ifindex;
     u64 ts, *tsp, delta;
+    struct latency_t lat = {};
 
     u64 recv_key = 2;
 
@@ -21,8 +30,10 @@ int do_recv(struct pt_regs *ctx, struct sk_buff *skb)
     } else if (dev_index == INNER_DEV_INDEX) {
         tsp = start.lookup(&recv_key);
         if (tsp != 0) {
-            delta = bpf_ktime_get_ns() - *tsp;
-            bpf_trace_printk("recv latency: %llu \\n ", delta);
+            lat.ts = bpf_ktime_get_ns();
+            lat.ns = lat.ts - *tsp;
+            lat.dir = recv_key;
+            events.perf_submit(ctx, &lat, sizeof(lat));
         }
         start.delete(&recv_key);
     }
@@ -34,6 +45,7 @@ int do_send(struct pt_regs *ctx, struct sk_buff *skb)
 {
     int dev_index = skb->dev->ifindex;
     u64 ts, *tsp, delta;
+    struct latency_t lat = {};
 
     u64 send_key = 1;
 
@@ -43,8 +55,10 @@ int do_send(struct pt_regs *ctx, struct sk_buff *skb)
     } else if (dev_index == OUTER_DEV_INDEX) {
         tsp = start.lookup(&send_key);
         if (tsp != 0) {
-            delta = bpf_ktime_get_ns() - *tsp;
-            bpf_trace_printk("send latency: %llu \\n ", delta);
+            lat.ts = bpf_ktime_get_ns();
+            lat.ns = lat.ts - *tsp;
+            lat.dir = send_key;
+            events.perf_submit(ctx, &lat, sizeof(lat));
         }
         start.delete(&send_key);
     }
@@ -62,7 +76,18 @@ b = BPF(text=prog)
 
 b.attach_kprobe(event="__netif_receive_skb_core", fn_name="do_recv")
 b.attach_kprobe(event="dev_queue_xmit", fn_name="do_send")
+
+class Latency(ct.Structure):
+    _fields_ = [("ts", ct.c_ulonglong),
+                ("ns", ct.c_ulonglong),
+                ("dir", ct.c_ulong)]
+def print_event(cpu, data, size):
+    event = ct.cast(data, ct.POINTER(Latency)).contents
+    print("ts: %d, ns: %d, dir: %d" % (event.ts, event.ns, event.dir))
+
+b["events"].open_perf_buffer(print_event)
+
 print("Watching for latencies")
 
-
-b.trace_print()
+while 1:
+    b.perf_buffer_poll()
